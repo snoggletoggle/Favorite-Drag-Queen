bf050cff7f233038f29468ec0832d3eae10c0d37
William Dayton <chrisdayt@gmail.com>
2017-06-21 23:25:41 -0500

    working drag queen posting, moved images out of season folders

--- script.js
+++ script.js
@@ -6,97 +6,102 @@ var checkboxes = [];
 const numSeasons = 9;
 
 $(document).ready(function() {
-	//sets up elements by hiding/disabling/checking
-	setup();
+  //sets up elements by hiding/disabling/checking
+  setup();
 });
 
-function setup(){
-	$('#helpContent').hide();
-	$('#all').prop('checked', true);
-	for(var i = 1; i <= numSeasons; i++){
-		checkboxes.push($('#s' + i));
-		checkboxes[i-1].prop('disabled', true);
-	}
-	$.getJSON('queens.json', function(data){
-		jsonqueens = JSON.parse(JSON.stringify(data));
-	})
-	$('#left').hide();
-	$('#right').hide();
+function setup() {
+  $('#helpContent').hide();
+  $('#all').prop('checked', true);
+  for (var i = 1; i <= numSeasons; i++) {
+    checkboxes.push($('#s' + i));
+    checkboxes[i - 1].prop('disabled', true);
+  }
+  $.getJSON('queens.json', function(data) {
+    jsonqueens = JSON.parse(JSON.stringify(data));
+  })
+  $('#left').hide();
+  $('#right').hide();
 }
 
-function clickAll(){
-	if($('#all').is(':checked')){
-		for(var i = 0; i < checkboxes.length; i++){
-			checkboxes[i]
-				.prop('checked', false)
-				.prop('disabled', true);
-		}
-	}else{
-		for(var i = 0; i < checkboxes.length; i++){
-			checkboxes[i]
-				.prop('disabled', false);
-		}
-	}
+function clickAll() {
+  if ($('#all').is(':checked')) {
+    for (var i = 0; i < checkboxes.length; i++) {
+      checkboxes[i]
+        .prop('checked', false)
+        .prop('disabled', true);
+    }
+  } else {
+    for (var i = 0; i < checkboxes.length; i++) {
+      checkboxes[i]
+        .prop('disabled', false);
+    }
+  }
 };
 
-function start(){
-	queens = [];
-	if(
-  $('#all').is(':checked') ||(
-	$('#s1').is(':checked') &&
-	$('#s2').is(':checked') &&
-	$('#s3').is(':checked') &&
-	$('#s4').is(':checked') &&
-	$('#s5').is(':checked') &&
-	$('#s6').is(':checked') &&
-	$('#s7').is(':checked') &&
-	$('#s8').is(':checked') &&
-	$('#s9').is(':checked')
-	)){
-		for(var i = 1; i <= numSeasons; i++){
-			for(var j = 0; j < (jsonqueens[i-1][1].length); j++){
-				queens.push(jsonqueens[i-1][1][j]);
-			}
-		}
-		startGame();
-	}
-	else if(
-	$('#s1').is(':checked') ||
-	$('#s2').is(':checked') ||
-	$('#s3').is(':checked') ||
-	$('#s4').is(':checked') ||
-	$('#s5').is(':checked') ||
-	$('#s6').is(':checked') ||
-	$('#s7').is(':checked') ||
-	$('#s8').is(':checked') ||
-	$('#s9').is(':checked')){
-		for(var i = 1; i <= numSeasons; i++){
-				if($('#s' + i).is(':checked')){
-					for(var j = 0; j < (jsonqueens[i-1][1].length); j++){
-						queens.push(jsonqueens[i-1][1][j]);
-					}
-				}
-		console.log(queens);
-		startGame();
-		}
-	}
-	else{
-		$('#title').html('Invalid season choices! Pick at least one!');
-	}
+function start() {
+  queens = [];
+  if (
+    $('#all').is(':checked') || (
+      $('#s1').is(':checked') &&
+      $('#s2').is(':checked') &&
+      $('#s3').is(':checked') &&
+      $('#s4').is(':checked') &&
+      $('#s5').is(':checked') &&
+      $('#s6').is(':checked') &&
+      $('#s7').is(':checked') &&
+      $('#s8').is(':checked') &&
+      $('#s9').is(':checked')
+    )) {
+    for (var i = 1; i <= numSeasons; i++) {
+      for (var j = 0; j < (jsonqueens[i - 1][1].length); j++) {
+        queens.push(jsonqueens[i - 1][1][j]);
+      }
+    }
+    game();
+  } else if (
+    $('#s1').is(':checked') ||
+    $('#s2').is(':checked') ||
+    $('#s3').is(':checked') ||
+    $('#s4').is(':checked') ||
+    $('#s5').is(':checked') ||
+    $('#s6').is(':checked') ||
+    $('#s7').is(':checked') ||
+    $('#s8').is(':checked') ||
+    $('#s9').is(':checked')) {
+    for (var i = 1; i <= numSeasons; i++) {
+      if ($('#s' + i).is(':checked')) {
+        for (var j = 0; j < (jsonqueens[i - 1][1].length); j++) {
+          queens.push(jsonqueens[i - 1][1][j]);
+        }
+      }
+      console.log(queens);
+    }
+    game();
+  } else {
+    $('#title').html('Invalid season choices! Pick at least one!');
+  }
 }
 
-function startGame(){
-	var randomLeft = Math.floor(Math.random() * queens.length);
-	var sameRight = true;
-	var randomRight;
-	while(sameRight){
-		randomRight = Math.floor(Math.random() * queens.length);
-		if(randomRight !== randomLeft){
-			sameRight = false;
-		}
-	}
-	$('#left').html('');
-	$('#left').html('');
+function game() {
+  var randomLeft = Math.floor(Math.random() * queens.length);
+  var sameRight = true;
+  var randomRight;
+  var seasonLeft = '';
+  var seasonRight = '';
+  while (sameRight) {
+    randomRight = Math.floor(Math.random() * queens.length);
+    if (randomRight !== randomLeft) {
+      sameRight = false;
+    }
+  }
+
+  $('#left')
+    .html('<img class="img-responsive" src="images/' + queens[randomLeft][1] + '">')
+    .show();
+  $('#right')
+    .html('<img class="img-responsive" src="images/' + queens[randomRight][1] + '">')
+    .show();
 
 };
 
@@ -104,12 +109,12 @@ function startGame(){
 $('#startButton').on('click', start);
 $('#all').on('click', clickAll);
 
-$('#helpButton').on('click', function(){
-	if($('#helpButton').html() === 'Help'){
-		$('#helpButton').html('Hide Help');
-		$('#helpContent').show();
-	}else{
-		$('#helpButton').html('Help');
-		$('#helpContent').hide();
-	}
+$('#helpButton').on('click', function() {
+  if ($('#helpButton').html() === 'Help') {
+    $('#helpButton').html('Hide Help');
+    $('#helpContent').show();
+  } else {
+    $('#helpButton').html('Help');
+    $('#helpContent').hide();
+  }
 });
